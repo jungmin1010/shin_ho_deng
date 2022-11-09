2e854249a4f3f705ea895b801b351f598162501a
jungmin1010 <117245564+jungmin1010@users.noreply.github.com>
2022-11-09 11:29:56 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
-let strip = neopixel.create(DigitalPin.P1, 1, NeoPixelMode.RGB)
+input.onButtonPressed(Button.A, function () {
+    OLED.clear()
+})
+input.onButtonPressed(Button.B, function () {
+    OLED.clear()
+    number = 0
+})
+let number = 0
+OLED.init(128, 64)
+pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
 basic.forever(function () {
-    if (50 > smarthome.ReadLightIntensity(AnalogPin.P2)) {
-        strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    } else {
-        strip.showColor(neopixel.colors(NeoPixelColors.White))
+	
+})
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P16) == 0) {
+        OLED.clear()
+        number += 5
     }
+    OLED.drawLoading(number)
 })
